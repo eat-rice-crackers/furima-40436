82a1110b860934e63a458f5a91ae28acffb41527
eat-rice-crackers <164855870+eat-rice-crackers@users.noreply.github.com>
2024-06-16 16:45:32 +0900

    rubocop実行

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :birth_date, presence: true
+
+  has_many :items
+  has_many :purchase_records
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Your password must contain both letters and numbers'
+end
