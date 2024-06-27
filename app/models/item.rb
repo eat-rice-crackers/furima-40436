82a3f95e15a13098cdd05405a40ddb2ba3ab921c
eat-rice-crackers <164855870+eat-rice-crackers@users.noreply.github.com>
2024-06-27 10:40:35 +0900

    商品出品機能　修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   belongs_to_active_hash :category_info
   belongs_to_active_hash :items_condition
