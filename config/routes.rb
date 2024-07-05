08259531b2a1cb8dcf4df8e1674f173f57daa004
eat-rice-crackers <164855870+eat-rice-crackers@users.noreply.github.com>
2024-07-05 21:05:34 +0900

    商品情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  root to: "items#index"
  devise_for :users
- resources :items, only: [:index, :new, :create, :show, :destroy, :edit]
+ resources :items, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 end
