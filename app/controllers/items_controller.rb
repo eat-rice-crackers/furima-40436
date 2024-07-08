0b2714f071befedbfe3846b8340d25b43c81b740
eat-rice-crackers <164855870+eat-rice-crackers@users.noreply.github.com>
2024-07-08 12:22:25 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -29,6 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy if current_user.id == @item.user_id
+    redirect_to root_path
   end
 
   def update
